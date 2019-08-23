5d8efb993cc2b4e0313793abb74d630b09be16e4
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-08-23 10:58:15 -0700

    working js will return beep, boop, and dave etc. respectively

--- jscripts/scripts.js
+++ jscripts/scripts.js
@@ -6,10 +6,20 @@ $(document).ready(function(){
   });
 });
 
-//BL.. lets get down to buisness...
+//BL.. lets get down to buisness... buisness comes first...
 function beepBoop(userInput){
   var number= parseInt(userInput);
-  
+  var indexOne = String(number).indexOf(1);
+  var indexTwo = String(number).indexOf(2);
+  var indexThree = String(number).indexOf(3);
+  if( indexOne !== (-1)){
   number = "beep"
+  }
+  else if( indexTwo !== (-1)){
+  number = "boop"
+  }
+  else if( indexThree !== (-1)){
+  number = "I'm sorry, Dave, but we just can't allow for that kind of behavior."
+  }
   return number;
 }
