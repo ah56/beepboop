0db95000e910bbe8b73a17ac8a69bbf8dad20255
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-08-23 10:38:18 -0700

    the form will return beep

--- jscripts/scripts.js
+++ jscripts/scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
-
-
+  $("form#numberform").submit(function(event){
+    event.preventDefault();
+    var userInput = $("#inputnumber").val();
+    $("#output").text(beepBoop(userInput));
+  });
 });
+
+//BL.. lets get down to buisness...
+function beepBoop(userInput){
+  var number= parseInt(userInput);
+  
+  number = "beep"
+  return number;
+}
